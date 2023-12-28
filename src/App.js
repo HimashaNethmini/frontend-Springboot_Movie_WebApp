a7d40c2de46455f6c58d46eecfffcdb00c2903dc
HimashaNethmini <himashanethmini05@gmail.com>
2023-12-28 18:25:52 +0530

    get all movies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import './App.css';
+import "./App.css";
+import api from "./api/axiosConfig";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [movies, setMovies] = useState();
+
+  //get all movies
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+
+      setMovies(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+
+      setReviews(singleMovie.reviews);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <div className="App">
-      
-    </div>
+  <div className="App">
+
+  </div>
   );
 }
 
